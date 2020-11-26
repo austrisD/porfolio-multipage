eb2d7e50795bea23c5baf2b69b596d805efd9403
austrisD <austris.daugulis@gmail.com>
2020-11-26 15:11:14 +0200

    Redisign-Change

--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -1,19 +1,11 @@
-import React, { useState } from "react";
-import lineBG from "../../globalAssets/gradientsLine.svg";
+import React from "react";
 import "./Greeting.scss";
 
 const Greeting = () => {
-  const [AddClassName, setAddClassName] = useState('');
-  setTimeout(() => {
-    setAddClassName("headerTop");
-  }, 3000);
   return (
-    <header
-      className={`greeting ${AddClassName}`}
-      style={{ backgroundImage: `url(${lineBG})` }}
-    >
+    <header className="greeting" >
       <h1 className="greeting__name">Austris Daugulis</h1>
-      <h2 className="greeting__jobTitle">Front-end developer</h2>
+      <h2 className="greeting__title">Front-end developer</h2>
     </header>
   );
 };
