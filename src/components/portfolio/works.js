0bc9c2a24d5f9f3433c8e0f1d536099cd871b373
austrisD <austris.daugulis@gmail.com>
2020-11-30 11:27:07 +0200

    works data

---
+++ src/components/portfolio/works.js
@@ -0,0 +1,66 @@
+export const works_data = [
+  {
+    workGroups: "WebSkola.lv",
+    start: "feb",
+    end: "July",
+    works: [
+      {
+        work_name: "1th site",
+        img: "./webpage__prew/webskolalv_prew.jpg",
+        url: "https://github.com/austrisD/darbi/tree/master/landing-Page",
+        tech: ["Html", "css", "Responsive"],
+      },
+      {
+        work_name: "Landing page/profile",
+        img: "./webpage__prew/profile__prew.jpg",
+        url: "https://github.com/austrisD/darbi/tree/master/landing-Page",
+        tech: ["Html", "scss", "Responsive", "Jekyll"],
+      },
+      {
+        work_name: "Landing page/SignUp",
+        img: "./webpage__prew/SignUp__prew.jpg",
+        url: "https://github.com/austrisD/darbi/tree/master/landing-Page",
+        tech: ["Html", "scss", "Responsive", "Jekyll"],
+      },
+      {
+        work_name: "Landing page/about",
+        img: "./webpage__prew/landing__prew.jpg",
+        url: "https://github.com/austrisD/darbi/tree/master/landing-Page",
+        tech: ["Html", "scss", "Responsive", "Jekyll"],
+      },
+      {
+        work_name: "toDoList",
+        img: "./webpage__prew/todolistPrew.jpg",
+        url:
+          "https://github.com/austrisD/darbi/tree/master/4th-project%3DToDOList",
+        tech: ["Html", "scss", "Responsive", "Bootstrap", "jQuery"],
+      },
+    ],
+  },
+  {
+    workGroups: "Self Training",
+    start: "aug",
+    end: "nov",
+    works: [
+      {
+        work_name: "Portfolio V:0.1",
+        img: "./webpage__prew/porfolio_v.1.png",
+        url: "https://github.com/austrisD/darbi/tree/master/my-site",
+        tech: ["Jsx", "scss", "Responsive", "React"],
+      },
+      {
+        work_name: "Ducati page",
+        img: "./webpage__prew/dacati__page.png",
+        url: "https://github.com/austrisD/darbi/tree/master/moto_page",
+        tech: ["Jsx", "scss", "Responsive", "React"],
+      },
+
+      {
+        work_name: "Mogo_page",
+        img: "./webpage__prew/Mogo__prew.png",
+        url: "https://github.com/austrisD/darbi/tree/master/MoGoPage",
+        tech: ["Html", "scss"],
+      },
+    ],
+  },
+];
