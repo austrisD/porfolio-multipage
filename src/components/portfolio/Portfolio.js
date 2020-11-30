bbb0900c820ae3f7ed777395afa93e04bd7bb577
austrisD <austris.daugulis@gmail.com>
2020-11-30 19:44:13 +0200

    work section

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,12 +1,52 @@
 import React from "react";
+import { works_data } from "./works_data";
 import "./Portfolio.scss";
+import eclipse from "./ellipse.svg";
 
 const Portfolio = () => {
- 
+  const mapData = works_data.map((value, index) => {
+    console.log(value.workGroups);
+    return (
+      <div key={index} className="portfolio__work__group">
+        <div className="portfolio__group__name">{value.workGroups}</div>
+        <div className="portfolio__works">
+          {value.works.map((value, index) => {
+            return (
+              <div className="portfolio__works_hover_content">
+                <p className="work_name">{value.work_name}</p>
+                <img key={index} src={value.img} alt={value.work_name} />
+              </div>
+            );
+          })}
+        </div>
+        <div className="portfolio__work__group_dates">
+          <div
+            className="circle dates_start"
+            style={{ backgroundImage: `url(${eclipse})` }}
+        >{value.start}</div>
+          <div
+            className="circle dates_end"
+            style={{ backgroundImage: `url(${eclipse})` }}
+        >{value.end}</div>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div id="portfolio" className="portfolio">
       <h3>Portfolio</h3>
-      <div className="elements"></div>
+
+      <div className="portfolio__container">
+        <div
+          className="portfolio__year"
+          style={{ backgroundImage: `url(${eclipse})` }}
+        >
+          2020
+        </div>
+        {mapData}
+        <div className="portfolio__work__group"></div>
+      </div>
     </div>
   );
 };
--- src/components/portfolio/works_data.js
+++ src/components/portfolio/works_data.js
@@ -1,3 +1,13 @@
+import img_webskola from"./webpage__prew/webskolalv_prew.jpg";
+import img_landing_profile from "./webpage__prew/landing__prew.jpg";
+import img_landing_signUp from "./webpage__prew/SignUp__prew.jpg";
+import img_landing_about from "./webpage__prew/profile__prew.jpg"
+import img_todolist from "./webpage__prew/todolistPrew.jpg";
+import img_porfolio_v1 from "./webpage__prew/porfolio_v.1.png";
+import img_moto_page from "./webpage__prew/dacati__page.png";
+import img_mogo_page from "./webpage__prew/Mogo__prew.png";
+
+
 export const works_data = [
   {
     workGroups: "WebSkola.lv",
@@ -6,31 +16,31 @@ export const works_data = [
     works: [
       {
         work_name: "1th site",
-        img: "./webpage__prew/webskolalv_prew.jpg",
+        img: img_webskola,
         url: "https://github.com/austrisD/darbi/tree/master/landing-Page",
         tech: ["Html", "css", "Responsive"],
       },
       {
         work_name: "Landing page/profile",
-        img: "./webpage__prew/profile__prew.jpg",
+        img: img_landing_profile,
         url: "https://github.com/austrisD/darbi/tree/master/landing-Page",
         tech: ["Html", "scss", "Responsive", "Jekyll"],
       },
       {
         work_name: "Landing page/SignUp",
-        img: "./webpage__prew/SignUp__prew.jpg",
+        img: img_landing_signUp,
         url: "https://github.com/austrisD/darbi/tree/master/landing-Page",
         tech: ["Html", "scss", "Responsive", "Jekyll"],
       },
       {
         work_name: "Landing page/about",
-        img: "./webpage__prew/landing__prew.jpg",
+        img: img_landing_about,
         url: "https://github.com/austrisD/darbi/tree/master/landing-Page",
         tech: ["Html", "scss", "Responsive", "Jekyll"],
       },
       {
         work_name: "toDoList",
-        img: "./webpage__prew/todolistPrew.jpg",
+        img: img_todolist,
         url:
           "https://github.com/austrisD/darbi/tree/master/4th-project%3DToDOList",
         tech: ["Html", "scss", "Responsive", "Bootstrap", "jQuery"],
@@ -44,20 +54,20 @@ export const works_data = [
     works: [
       {
         work_name: "Portfolio V:0.1",
-        img: "./webpage__prew/porfolio_v.1.png",
+        img: img_porfolio_v1,
         url: "https://github.com/austrisD/darbi/tree/master/my-site",
         tech: ["Jsx", "scss", "Responsive", "React"],
       },
       {
         work_name: "Ducati page",
-        img: "./webpage__prew/dacati__page.png",
+        img: img_moto_page,
         url: "https://github.com/austrisD/darbi/tree/master/moto_page",
         tech: ["Jsx", "scss", "Responsive", "React"],
       },
 
       {
         work_name: "Mogo_page",
-        img: "./webpage__prew/Mogo__prew.png",
+        img: img_mogo_page,
         url: "https://github.com/austrisD/darbi/tree/master/MoGoPage",
         tech: ["Html", "scss"],
       },
